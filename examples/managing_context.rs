3ec67f56ae4ec1429b91830784d02f3865b4d7fb
Allex Jensen <allex.jen@outlook.com>
2022-07-28 19:07:46 -0600

    Add copyright header and description

--- examples/managing_context.rs
+++ examples/managing_context.rs
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2021 QuantumBadger
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+// Minimal example on how to use Speedy2D while managing the GL context yourself.
+// Uses glutin for cross-platform window creation and does not do any error handling.
+
 use glutin::dpi::PhysicalSize;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
