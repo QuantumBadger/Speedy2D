1bfd2196bf6d20fa4c77f08ac7cbadc50d85f266
Dennis Orlando <dennis.orlando2002@gmail.com>
2023-04-05 20:27:35 +0200

    flagged get_canvas_size with #[cfg(feature = 'windowing')]

--- src/web.rs
+++ src/web.rs
@@ -449,6 +449,7 @@ impl WebCanvasElement
         &self.html_element
     }
 
+    #[cfg(feature = "windowing")]
     pub fn get_canvas_size(&self) -> UVec2
     {
         let width = self.canvas.width() as u32;
